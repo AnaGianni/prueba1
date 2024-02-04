d1c853d2b7b92de86c364330194ca2303fab2bda
Ana Gianni <giannianaclara@gmail.com>
2024-02-04 13:08:31 -0300

    Se le agrega una carpeta nueva

---
+++ PrimerProyectoGit/Pato4/Pato.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pato4
+{
+    public class Pato
+    {
+        public int pico { get; set; }
+
+        public string habitat { get; set; }
+
+        public Pato()
+        {
+
+        }
+
+        public void MostrarPato()
+        {
+            Console.WriteLine($"El pato tiene {pico} pico" +
+                $" y su habitat es el {habitat}");
+        }
+    }
+}
---
+++ PrimerProyectoGit/Pato4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Pato4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pato pato1 = new Pato();
+
+            pato1.pico = 1;
+            pato1.habitat = "lago";
+            pato1.MostrarPato();
+           
+            
+            Console.ReadKey();
+        }
+    }
+}
